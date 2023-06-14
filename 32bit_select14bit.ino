3bfd144f1bd965fd29c2ef103342cfd2329f4d2f
hanerdemhan <109851942+hanerdemhan@users.noreply.github.com>
2023-06-14 16:43:25 +0300

    Update 32bit_select14bit.ino

--- 32bit_select14bit.ino
+++ 32bit_select14bit.ino
@@ -1,30 +1,58 @@
-const int inputPin = 2; // D2 pini
+// pre setup - thermocouple
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+// Example creating a thermocouple instance with software SPI on any three
+// digital IO pins.
+#define MAXDO   12
+#define MAXCS   11
+#define MAXCLK  10
+
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 void setup() {
-  Serial.begin(9600); // Seri portu başlat
-  pinMode(inputPin, INPUT); // Giriş pini olarak ayarla
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
+  Serial.println("MAX31855 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+  Serial.print("Initializing sensor...");
+  if (!thermocouple.begin()) {
+    Serial.println("ERROR.");
+    while (1) delay(10);
+  }
+  Serial.println("DONE.");
 }
 
 void loop() {
-  // 32 bitlik giriş sinyalini okuma
-  unsigned long inputSignal = digitalReadSignal();
+  // read thermocouple
+  // basic readout test, just print the current temp
+  Serial.print("Internal Temp = ");
+  double int_temp = thermocouple.readInternal();
+  Serial.println(int_temp);
 
-  // D31 ile D14 arasındaki 18 biti ayıklama
-  unsigned int extractedBits = (inputSignal >> 14) & 0x0003FFFF;
-
-  // Ayıklanan bitleri seri portta gösterme
-  Serial.println(extractedBits);
-
-  delay(1000); // İşlem arasında 1 saniye gecikme
-}
-
-unsigned long digitalReadSignal() {
-  unsigned long signal = 0;
-  for (int i = 0; i < 32; i++) {
-    int bitValue = digitalRead(inputPin); // D2 pinden bit değerini oku
-    signal = (signal << 1) | bitValue; // Okunan biti sinyale ekle
-    delayMicroseconds(1); // Küçük bir gecikme
+  double c = thermocouple.readCelsius();
+  if (isnan(c)) {
+    Serial.println("Something wrong with thermocouple!");
+  } else {
+    Serial.print("C (Decimal) = ");
+    Serial.println(c);
+    Serial.print("C (Binary) = ");
+    Serial.println(floatToBinary(c));
   }
-  return signal;
+
+  delay(1000);
 }
 
+String floatToBinary(float value) {
+  byte *ptr = (byte *)&value;
+  String binaryString = "";
+  for (int i = sizeof(float) - 1; i >= 0; i--) {
+    for (int j = 7; j >= 0; j--) {
+      binaryString += (*ptr >> j) & 1 ? '1' : '0';
+    }
+    ptr++;
+  }
+  return binaryString;
+}
